cba8e5fab2a5560064f49679083f9e131401762a
kyokutyo <haruta@kyokutyo.com>
2014-12-02 22:24:58 +0900

    jQuery を導入して、Flickr API を叩く

--- webapp2/static/js/app.js
+++ webapp2/static/js/app.js
@@ -1,10 +1,48 @@
 (function() {
     'use strict';
 
+    var api_url = 'https://api.flickr.com/services/rest/';
+    var params = {
+        method: 'flickr.photos.search',
+        api_key: '17570926bf4df161849251ae5cdfaa1b',
+        user_id: '112437165@N04',
+        format: 'json',
+        nojsoncallback: '1'
+    };
+
     var App = React.createClass({
+        getInitialState: function() {
+            return {
+                total: undefined
+            };
+        },
+        setPhotoState: function(data) {
+            this.setState({
+                total: parseInt(data.photos.total, 10)
+            });
+        },
+        componentDidMount: function() {
+            var that = this;
+
+            $.ajax(api_url, {
+                dataType: 'json',
+                data: params,
+                success: that.setPhotoState,
+                error: function() {
+                    console.error();
+                }
+            });
+        },
         render: function() {
+            var link;
+
+            if(typeof this.state.total !== 'undefined') {
+                link = (
+                    <a href={api_url}>{this.state.total}</a>
+                );
+            }
             return (
-                <p>あああ</p>
+                <p>{link}</p>
             );
         }
     });
